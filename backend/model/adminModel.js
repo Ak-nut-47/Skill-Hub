15e40124011e7a88ac3c4baffd301c1b267105e3
Sanketh-Ganiga <sankethganiga888@gmail.com>
2023-07-20 21:50:07 +0530

    search,filter,sort,pagination,update,delete

---
+++ backend/model/adminModel.js
@@ -0,0 +1,9 @@
+const mongoose=require("mongoose")
+
+const adminSchema=mongoose.Schema({
+    username:{type:String,required:true},
+    email:{type:String,required:true},
+    phone:{type:Number,required:true},
+    password:{type:String,required:true}
+})
+module.exports=mongoose.model("admin",adminSchema)
